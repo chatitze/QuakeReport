8b77e5abd076400ac0322d10912d51c8e6db1509
chatitzem <chatitze.moumin@gmail.com>
2021-02-21 19:28:24 +0200

    1.9 Add additional explanation of classes, attributes, and methods

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -16,16 +16,37 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * An {@link EarthquakeAdapter} knows how to create a list item layout for each earthquake
+ * in the data source (a list of {@link Earthquake} objects).
+ *
+ * These list item layouts will be provided to an adapter view like ListView
+ * to be displayed to the user.
+ */
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
     private static final String LOG_TAG = EarthquakeAdapter.class.getSimpleName();
 
+    /**
+     * The part of the location string from the USGS service that we use to determine
+     * whether or not there is a location offset present ("5km N of Cairo, Egypt").
+     */
     private static final String LOCATION_SEPARATOR = " of ";
 
+    /**
+     * Constructs a new {@link EarthquakeAdapter}.
+     *
+     * @param context of the app
+     * @param earthquakes is the list of earthquakes, which is the data source of the adapter
+     */
     public EarthquakeAdapter(@NonNull Activity context,  @NonNull ArrayList<Earthquake> earthquakes) {
         super(context, 0, earthquakes);
     }
 
+    /**
+     * Returns a list item view that displays information about the earthquake at the given position
+     * in the list of earthquakes.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
